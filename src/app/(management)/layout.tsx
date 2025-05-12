20f0dc68e1ff858ca94204ac1fd1da80a482e409
Gianluca Paschoalotti <gianluca170703@gmail.com>
2025-05-11 22:29:31 -0300

    Criação de um dark mode

--- src/app/(management)/layout.tsx
+++ src/app/(management)/layout.tsx
@@ -4,9 +4,9 @@ export default function managementLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex items-center justify-center h-screen bg-linear-to-r from-cyan-600 to-blue-900">
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-cyan-600 to-blue-900 dark:from-blue-900 dark:to-blue-700">
       <main>
-        <div className="bg-white p-8 rounded-2xl shadow-xl">{children}</div>
+        <div className="bg-white dark:bg-gray-800 p-8 rounded-2xl shadow-xl">{children}</div>
       </main>
     </div>
   );
